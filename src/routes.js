55b1e989c4697ab9413cd45c2c9b8cdfab827a66
Higor Rodrigues <higor_1992@hotmail.com>
2022-12-14 20:30:36 -0300

    adicionado nodemon

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,4 @@
-const { Router } = require('express')
+import { Router } from 'express'
 
 const routes = new Router()
 
@@ -6,5 +6,5 @@ routes.get('/', (request, response) => {
     return response.json({message: "Hello World"})
 })
 
-module.exports = routes
+export default routes
 
