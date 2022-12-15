07114d44874b1f2e1291767b91bebe7ec35f2441
Higor Rodrigues <higor_1992@hotmail.com>
2022-12-15 12:47:43 -0300

    enviando informações para o banco de dados

--- src/app.js
+++ src/app.js
@@ -1,22 +1,23 @@
 import express from 'express'
 import routes from './routes'
 
+
+import './database'
 class App {
-    constructor(){
-        this.app = express()
+  constructor () {
+    this.app = express()
 
-        this.middlewares()
-        this.routes()
-    }
-    
+    this.middlewares()
+    this.routes()
+  }
 
-    middlewares(){
-        this.app.use(express.json())
-    }
+  middlewares () {
+    this.app.use(express.json())
+  }
 
-    routes(){
-        this.app.use(routes)
-    }
+  routes () {
+    this.app.use(routes)
+  }
 }
 
 export default new App().app
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,20 @@
-import { Router } from 'express'
+import { Router } from "express"
+import { v4 } from 'uuid'
+
+import User from './app/models/User'
 
 const routes = new Router()
 
-routes.get('/', (request, response) => {
-    return response.json({message: "Hello World"})
-})
+routes.get("/", async (request, response) => {
+  const user = await User.create({
+    id: v4(),
+    name: 'Higor Rodrigues',
+    email: 'higorrodrigues@email.com',
+    password_hash: '23asd234',
+    admin: true,
 
-export default routes
+  })
+  return response.json(user)
+});
 
+export default routes;
