a15d747386b11df8746b554040c02f4299497856
Higor Rodrigues <higor_1992@hotmail.com>
2022-12-22 12:50:43 -0300

    adicionado confirmação de email's validos e cadastrados, e confirmação de senhas validas ou cadastradas

--- src/app/models/controllers/UserController.js
+++ src/app/models/controllers/UserController.js
@@ -19,7 +19,7 @@ class UserController{
         const schema = Yup.object().shape({
             name: Yup.string().required(),
             email: Yup.string().email(),
-            password_hash: Yup.string().required().min(6),
+            password: Yup.string().required().min(6),
             admin: Yup.boolean(),
         })
        
@@ -35,7 +35,7 @@ class UserController{
             return response.status(400).json({ error: err.errors })
         }
 
-        const { name, email, password_hash, admin  } = request.body
+        const { name, email, password, admin  } = request.body
 
         const userExists = await User.findOne({
             where: { email },
@@ -51,7 +51,7 @@ class UserController{
             id: v4(),
             name,
             email,
-            password_hash,
+            password,
             admin,
         })
 
