6bd0091837d1d0b829d48ec687bd380fefe261dd
Higor Rodrigues <higor_1992@hotmail.com>
2022-12-22 15:11:08 -0300

    adicionado rota para adicionar produtos

---
+++ src/app/models/controllers/ProductController.js
@@ -0,0 +1,42 @@
+import * as Yup from 'yup'
+import Product from '../Product'
+
+class ProductController{
+    async store(request, response) {
+        const  schema = Yup.object().shape({
+            name: Yup.string().required(),
+            price: Yup.number().required(),
+            category: Yup.string().required(),
+        })
+        
+
+        try {
+            await schema.validateSync(request.body, { abortEarly: false })
+        } catch(err){
+            return response.status(400).json({ error: err.errors })
+        }
+
+        const { filename: path } = request.file
+        const { name, price, category } = request.body
+
+        const product = await Product.create({
+            name,
+            price,
+            category,
+            path,
+        })
+        
+        return response.json(product)
+    }
+
+    async index(request, response) {
+        const products = await Product.findAll()
+        
+        return response.json(products)
+}
+
+}
+
+
+
+export default new ProductController()
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,22 @@
 import { Router } from "express"
+import multer from "multer"
+import multerConfig from "./config/multer"
+
 import SessionController from "./app/models/controllers/SessionController"
+import ProductController from "./app/models/controllers/ProductController"
 
 import UserController from './app/models/controllers/UserController'
+
+const upload = multer(multerConfig)
+
 const routes = new Router()
 
 routes.post("/users", UserController.store)
 
 routes.post("/sessions", SessionController.store)
+
+routes.post("/products", upload.single('file'), ProductController.store)
+routes.get("/products", ProductController.index)
 
 
 export default routes;
