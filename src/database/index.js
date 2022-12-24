b360d460ffdbd6c247b4b7ab47b3966551fb9694
Higor Rodrigues <higor_1992@hotmail.com>
2022-12-24 11:28:09 -0300

    criando relacionamento entre tabelas e alterando Controller de produtos

--- src/database/index.js
+++ src/database/index.js
@@ -15,7 +15,10 @@ class Database {
 
     init() {
         this.connection = new Sequelize(configDatabase)
-        models.map( model => model.init(this.connection))
+        models
+        .map((model) => model.init(this.connection))
+        .map( 
+            (model) => model.associate && model.associate(this.connection.models))
     }
 }
 
