3c38d3bc7ccfc12880c4a99bbe1c9dbabe19ceca
Higor Rodrigues <higor_1992@hotmail.com>
2022-12-24 13:14:35 -0300

    instando mongo criando conexão e criação do Scheme de pedidos

--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,5 @@
 import Sequelize from 'sequelize'
+import mongoose from 'mongoose'
 
 import Product from '../app/models/Product'
 import User from '../app/models/User'
@@ -7,10 +8,10 @@ import Category from '../app/models/Category'
 import configDatabase from '../config/database'
 
 const models = [User, Product, Category]
-
 class Database {
     constructor(){
         this.init()
+        this.mongo()
     }
 
     init() {
@@ -19,6 +20,18 @@ class Database {
         .map((model) => model.init(this.connection))
         .map( 
             (model) => model.associate && model.associate(this.connection.models))
+    }
+
+    mongo() {
+        mongoose.set('strictQuery', false);
+
+       this.mongoConnection = mongoose.connect(
+            'mongodb://localhost:27017/codeburber', 
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+            }
+            )
     }
 }
 
