d00d39c3643e95c0ac770536433ca1dd1cc0295e
Higor Rodrigues <higor_1992@hotmail.com>
2022-12-28 14:58:03 -0300

    adicionado a coluna de promoção

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -5,13 +5,14 @@ import User from '../models/User'
 
 class ProductController{
     async store(request, response) {
-        const  schema = Yup.object().shape({
+      try {
+         const  schema = Yup.object().shape({
             name: Yup.string().required(),
             price: Yup.number().required(),
             category_id: Yup.number().required(),
+            offer: Yup.boolean(),
         })
         
-
         try {
             await schema.validateSync(request.body, { abortEarly: false })
         } catch(err){
@@ -25,19 +26,22 @@ class ProductController{
         }
 
         const { filename: path } = request.file
-        const { name, price, category_id } = request.body
+
+        const { name, price, category_id, offer } = request.body
 
         const product = await Product.create({
             name,
             price: price,
             category_id,
             path,
+            offer,
         })
         
         return response.json(product)
     } catch (err) {
         console.log(err)
     }
+}
 
     async index(request, response) {
         const products = await Product.findAll({
@@ -50,6 +54,60 @@ class ProductController{
         })
         
         return response.json(products)
+}
+
+async update(request, response) {
+    try {
+        const  schema = Yup.object().shape({
+        name: Yup.string(),
+        price: Yup.number(),
+        category_id: Yup.number(),
+        offer: Yup.boolean(),
+    })
+    
+    try {
+        await schema.validateSync(request.body, { abortEarly: false })
+    } catch(err){
+        return response.status(400).json({ error: err.errors })
+    }
+
+    const { admin: isAdmin} = await User.findByPk(request.userId)
+
+    if(!isAdmin){
+        return response.status(401).json()
+    }
+
+    const { id } = request.params
+
+    const product = await Product.findByPk(id)
+
+    if(!product){
+        return response.status(401).json({ error: "Make sure your product ID is correct"})
+    }
+
+    let path
+    if(request.file){
+        path = request.file.filename
+    }
+ 
+    const { name, price, category_id, offer } = request.body
+
+   await Product.update(
+    {
+        name,
+        price,
+        category_id,
+        path,
+        offer,
+    },
+    {
+        where: { id }
+    })
+    
+    return response.status(200).json()
+} catch (err) {
+    console.log(err)
+}
 }
 }
 
