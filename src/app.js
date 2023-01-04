a91132ac2d7dd38dcb0517ddfb8dec5112bf6efc
Higor Rodrigues <higor_1992@hotmail.com>
2023-01-04 13:10:31 -0300

    colocando o cors no lugar correto dentro do constructor

--- src/app.js
+++ src/app.js
@@ -7,10 +7,10 @@ import "./database";
 class App {
   constructor() {
     this.app = express();
+    this.app.use(cors());
 
     this.middlewares();
     this.routes();
-    this.app.use(cors());
   }
 
   middlewares() {
--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,46 +1,42 @@
-import * as Yup from 'yup'
-import jwt from 'jsonwebtoken'
-import authConfig from '../../config/auth'
-import User from '../models/User'
+import * as Yup from "yup";
+import jwt from "jsonwebtoken";
+import authConfig from "../../config/auth";
+import User from "../models/User";
 
 class SessionController {
-   async store(request, response){
-
+  async store(request, response) {
     const schema = Yup.object().shape({
-        email: Yup.string().email().required(),
-        password: Yup.string().required(),
-    })
+      email: Yup.string().email().required(),
+      password: Yup.string().required(),
+    });
 
     const userEmailOrPasswordIncorrect = () => {
-        return response
+      return response
         .status(400)
-        .json({error: 'Make sure your password or email are correct'})
-    }
-    if (!(await schema.isValid(request.body))) userEmailOrPasswordIncorrect()
-    
+        .json({ error: "Make sure your password or email are correct" });
+    };
+    if (!(await schema.isValid(request.body))) userEmailOrPasswordIncorrect();
 
-    const { email, password } = request.body
+    const { email, password } = request.body;
 
     const user = await User.findOne({
-        where: { email },
-    })
+      where: { email },
+    });
 
-    if (!user) userEmailOrPasswordIncorrect()
-    
+    if (!user) userEmailOrPasswordIncorrect();
 
-    if(!(await user.checkPassword(password))) userEmailOrPasswordIncorrect()
-    
+    if (!(await user.checkPassword(password))) userEmailOrPasswordIncorrect();
 
     return response.json({
-        id: user.id, 
-        email, 
-        name: user.name, 
-        admin: user.admin,
-        token: jwt.sign({ id: user.id, name: user.name }, authConfig.secret, {
-            expiresIn: authConfig.expiresIn, 
-        }),
-    })
-} 
+      id: user.id,
+      email,
+      name: user.name,
+      admin: user.admin,
+      token: jwt.sign({ id: user.id, name: user.name }, authConfig.secret, {
+        expiresIn: authConfig.expiresIn,
+      }),
+    });
+  }
 }
 
-export default new SessionController()
+export default new SessionController();
