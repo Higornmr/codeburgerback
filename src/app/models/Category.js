7e1179028b6def88598ea18435f36f7a317acb6d
Higor Rodrigues <higor_1992@hotmail.com>
2023-01-06 20:07:18 -0300

    corrigindo a rota da category e do products de 3000 para 3001

--- src/app/models/Category.js
+++ src/app/models/Category.js
@@ -1,24 +1,24 @@
 import Sequelize, { Model } from "sequelize";
 
 class Category extends Model {
-    static init(sequelize){
-        super.init(
-            {
-                name: Sequelize.STRING,  
-                path: Sequelize.STRING,   
-                url: {
-                    type: Sequelize.VIRTUAL,
-                    get(){
-                        return `http://localhost:3000/category-file/${this.path}`
-                    },
-                },      
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        path: Sequelize.STRING,
+        url: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            return `http://localhost:3001/category-file/${this.path}`;
+          },
         },
-        {
-            sequelize,
-        }
-        )
-        return this
-    }
+      },
+      {
+        sequelize,
+      }
+    );
+    return this;
+  }
 }
 
-export default Category
+export default Category;
